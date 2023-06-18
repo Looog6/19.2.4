65c07f8c997676967048b9481c3555f10cc41111
Looog6 <120652500+Looog6@users.noreply.github.com>
2023-06-18 16:43:49 +0400

    Update test.py

--- Calculator/test/test.py
+++ Calculator/test/test.py
@@ -10,25 +10,25 @@ class TestCalc:
         self.calc = Calculator()
 # Положительные тесты
     def test_multiply_success(self):
-        assert self.calc.multiply(5, 5) == 25
+        assert self.calc.multiply(7, 7) == 49
 
     def test_division_success(self):
-        assert self.calc.division(10, 5) == 2
+        assert self.calc.division(20, 5) == 4
 
     def test_subtraction_success(self):
-        assert self.calc.subtraction(5, 1) == 4
+        assert self.calc.subtraction(9, 5) == 4
 
     def test_adding_success(self):
         assert self.calc.adding(2, 2) == 4
 # Отрицателные тесты
     def test_multiply_unsuccess(self):
-        assert self.calc.multiply(5, 5) == 20
+        assert self.calc.multiply(7, 7) == 47
 
     def test_division_unsucces(self):
-        assert self.calc.division(10, 5) == 5
+        assert self.calc.division(20, 5) == 15
 
     def test_subtraction_unsuccess(self):
-        assert self.calc.subtraction(5, 1) == 5
+        assert self.calc.subtraction(9, 5) == 5
 
     def test_adding_unsuccess(self):
         assert self.calc.adding(2, 2) == 3
