d7cd5a182b9c207d866837e3d5c1868c22df4e0b
Looog6 <120652500+Looog6@users.noreply.github.com>
2023-06-18 14:27:31 +0400

    Update calc.py

--- Calculator/app/calc.py
+++ Calculator/app/calc.py
@@ -1,12 +1,12 @@
 class Calculator:
-   def multiply(self, x, y):
+   def multiply(self, x, y):      # умножение
        return x * y
 
-   def division(self, x, y):
+   def division(self, x, y):      # деление
        return x / y
 
-   def subtraction(self, x, y):
+   def subtraction(self, x, y):   # вычитание
        return x - y
 
-   def adding(self, x, y):
+   def adding(self, x, y):        # сложение
        return x + y
